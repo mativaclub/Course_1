67b19c9cc897b5c280db2a0d8cde9617bd82e4b8
mativaclub <96348077+mativaclub@users.noreply.github.com>
2022-02-02 00:04:18 +0200

    homework

---
+++ src/ru/skypro/Employee.java
@@ -0,0 +1,55 @@
+package ru.skypro;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private float salary;
+    private int department;
+    private static int id;
+   private static Integer counter = 1;
+
+    public Employee(String firstName, String lastName, String middleName, float salary, int department, int id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleName = middleName;
+        this.salary = salary;
+        this.department = department;
+        this.id = counter++;
+    }
+
+    @Override
+    public String toString() {
+        return "FirstName: " + firstName + ",  LastName: " + lastName + ",  MiddleName: "
+                + middleName + ",  Salary: " + salary + ", Department: " + department + ", " + id;
+    }
+    public static int getId() {
+        return id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getMiddleName() {
+        return middleName;
+    }
+    public float getSalary() {
+        return salary;
+    }
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+
+
+
+}
